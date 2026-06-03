bba8ba9325da7b5c9e9533b60dcb6440b1d45a99
MST <strmadalin@gmail.com>
2026-06-04 00:45:45 +0300

    fixes 2
    tests

--- playwright.config.js
+++ playwright.config.js
@@ -30,33 +30,12 @@ module.exports = defineConfig({
     screenshot: 'off',
   },
 
-  /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-/*
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
     
-    {
-      name: 'Mobile Chrome',
-      use: { ...devices['Pixel 5'] },
-    },
-    {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] },
-    },
-    /*
   ],
 
   /* Run your local dev server before starting the tests */
