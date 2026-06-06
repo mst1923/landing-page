1da7fb816e19fb69144db893a547e37a4403ccd1
MST <strmadalin@gmail.com>
2026-06-06 21:01:37 +0300

    Fix 5

--- playwright.config.js
+++ playwright.config.js
@@ -36,7 +36,8 @@ module.exports = defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-
+    
+    /*
     {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
@@ -47,7 +48,6 @@ module.exports = defineConfig({
       use: { ...devices['Desktop Safari'] },
     },
     
-    /*
     {
       name: 'Mobile Chrome',
       use: { ...devices['Pixel 5'] },
@@ -60,9 +60,9 @@ module.exports = defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-   webServer: {
-     command: 'npm run dev',
-     url: 'http://127.0.0.1:3000',
-     reuseExistingServer: !process.env.CI,
-   },
+   //webServer: {
+   //  command: 'npm run dev',
+   //  url: 'http://127.0.0.1:3000',
+   //  reuseExistingServer: !process.env.CI,
+   //},
 });
